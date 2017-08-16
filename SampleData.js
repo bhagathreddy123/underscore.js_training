c1f516db8c3b7f8decebc9fd62fab13073319aad
bhagath <bhagath@getcustomexposure.com>
2017-08-16 07:14:26 +0530

    _.each,_.size functions usage

---
+++ SampleData.js
@@ -0,0 +1,40 @@
+var students = [
+{
+	"firstname": "woody",
+
+	"lastname": "Johnson",
+	"grade": 12,
+
+	"midterm_score": 76,
+	"final_score": 85
+},
+{
+	"firstname": "bhagath",
+
+	"lastname": "reddy",
+	"grade": 5,
+
+	"midterm_score": 75,
+	"final_score": 85
+},
+{
+	"firstname": "nathan",
+
+	"lastname": "astile",
+	"grade": 10,
+
+	"midterm_score": 76,
+	"final_score": 85
+},
+{
+	"firstname": "rajesh",
+
+	"lastname": "chowan",
+	"grade": 9,
+
+	"midterm_score": 76,
+	"final_score": 85
+}
+
+]
+	
